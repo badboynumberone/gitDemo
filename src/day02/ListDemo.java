eee7e36d4e72d9cd94429558d4cf4dd8646a3768
dzf <1143636922@qq.com>
2019-09-12 02:18:05 +0800

    first-commit

---
+++ src/day02/ListDemo.java
@@ -0,0 +1,21 @@
+package day02;
+
+import java.util.LinkedList;
+
+/*
+* 集合
+*
+* ArrayList
+优点: 底层数据结构是数组，查询快，增删慢。
+缺点: 线程不安全，效率高
+
+LinkedList
+优点: 底层数据结构是链表，查询慢，增删快。
+缺点: 线程不安全，效率高
+* */
+public class ListDemo {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("");
+    }
+}
