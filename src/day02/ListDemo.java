e723b77a77d467a744c446de899d3bc30da4b520
dzf <1143636922@qq.com>
2019-09-12 10:59:05 +0800

    login第二次开发

--- src/day02/ListDemo.java
+++ src/day02/ListDemo.java
@@ -19,6 +19,7 @@ public class ListDemo {
     public static void main(String[] args) {
         System.out.println("这是在开发login登录功能,");
         System.out.println("登录功能开发完成，准备发布");
+        System.out.println("登录功能发现问题现在准备重新开发，开发出另外一版本");
     }
 
 }
